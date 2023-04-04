8cad0a76fd1e360588e67926b1095dc61498fd3e
eddy <eddy@stlwolf.com>
2023-04-04 20:59:03 +0900

    fix: Added background images for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -34,4 +34,17 @@ config.keys = {
   { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
 }
 
+config.window_background_image = ''
+
+config.window_background_image_hsb = {
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.2,
+
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  hue = 1.0,
+
+  -- You can adjust the saturation also.
+  saturation = 1.0,
+}
 return config
