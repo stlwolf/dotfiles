9a6ae1883ee2443bd286bdef426e15c1a5c6c53a
eddy <eddy@stlwolf.com>
2023-04-12 20:36:23 +0900

    fix: Updated hammerspoon script

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,3 +7,30 @@ end
 
 double_press.timeFrame = 0.5
 double_press.action = open_wezterm
+
+-- QuickTimeウィンドウの再生/一時停止をトグル
+function toggleQuickTimePlayback()
+    hs.notify.new({title="Hammerspoon", informativeText="toggleQuickTimePlayback called"}):send()
+    local script = [[
+        tell application "QuickTime Player"
+            set allDocs to every document
+            if (count of allDocs) > 0 then
+                repeat with eachDoc in allDocs
+                    if (playing of eachDoc) is true then
+                        pause eachDoc
+                    else
+                        play eachDoc
+                    end if
+                end repeat
+            end if
+        end tell
+    ]]
+    local _, _, stderr = hs.osascript.applescript(script)
+    if stderr and (type(stderr) == "string") and (string.len(stderr) > 0) then
+        hs.notify.new({title="Hammerspoon", informativeText="QuickTime Playback Toggle Error: " .. stderr}):send()
+    end
+end
+
+-- ショートカットキーの設定
+hs.hotkey.bind({"ctrl", "alt"}, "P", toggleQuickTimePlayback)
+
